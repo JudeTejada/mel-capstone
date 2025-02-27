ae2bb883c98ec6dad10dd5f07eae8481d4c2d425
Jude Tejada <judelaurentetejada@gmail.com>
2025-02-27 19:51:30 +0800

    remove eslint error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ const config = {
   ],
   rules: {
     "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
